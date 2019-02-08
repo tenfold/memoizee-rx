8d01d257d248dcbd05d919bb09af0c3cc3f547f6
Damian Kaczmarek <rush@rushbase.net>
2019-02-08 19:47:47 +0100

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-export function hello() {
-  return 'hello';
+import * as memoizee from 'memoizee';
+import { Observable, Subject } from 'rxjs';
+import { shareReplay, takeUntil } from 'rxjs/operators';
+
+type ObservableFunction<T> = (...args: any[]) => Observable<T>;
+export function memoizeeRx<T>(toWrap: ObservableFunction<T>, options: memoizee.Options = {}): ObservableFunction<T> {
+    const disposeSymbol = Symbol('___dispose');
+    const newOptions: memoizee.Options = Object.assign({
+        normalizer: (args: any[]) => {
+            return Array.from(args);
+        },
+    }, options, {
+        dispose(value: any) {
+            value[disposeSymbol]();
+        },
+    });
+
+    return memoizee(function(this: any, ...args: any[]) {
+        const result$ = toWrap.apply(this, args);
+        const expired$ = new Subject();
+        const sharedResult$ = result$.pipe(takeUntil(expired$), shareReplay());
+        (sharedResult$ as any)[disposeSymbol] = () => expired$.next();
+        return sharedResult$;
+    }, newOptions);
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,9 +1,56 @@
-import { hello } from '../src/index';
+import { concat, Observable, of } from 'rxjs';
+import { toArray } from 'rxjs/operators';
+import { memoizeeRx } from '../src/index';
 
-describe('index', () => {
-  it('should greet', () => {
-    const out = hello();
+const delay = (ms: number) => new Promise((resolve, reject) => setTimeout(resolve, ms));
 
-    expect(out).toEqual('hello');
+describe('memoizeeRx', () => {
+  it('should memoizee an rx stream', async () => {
+    const implementation = jest.fn((x: number) => {
+      return concat(of(x*2), of(x*3), of(x*4));
+    });
+    const f = memoizeeRx(implementation);
+
+    const out = await f(2).pipe(toArray()).toPromise();
+    const out2 = await f(2).pipe(toArray()).toPromise();
+
+    expect(out).toEqual([4, 6, 8]);
+    expect(out2).toEqual([4, 6, 8]);
+    expect(implementation).toHaveBeenCalledTimes(1);
+  });
+
+  it('should support timeout', async () => {
+    const implementation = jest.fn((x: number) => {
+      return concat(of(x*2), of(x*3), of(x*4));
+    });
+    const f = memoizeeRx(implementation, { maxAge: 1 });
+    const out = await f(2).pipe(toArray()).toPromise();
+    await delay(2);
+    const out2 = await f(2).pipe(toArray()).toPromise();
+
+    expect(out).toEqual([4, 6, 8]);
+    expect(out2).toEqual([4, 6, 8]);
+    expect(implementation).toHaveBeenCalledTimes(2);
+  });
+
+  it('should unsubscribe from source upon cleanup', async () => {
+    const onSubscribe = jest.fn();
+    const f = memoizeeRx(() => {
+      return new Observable(subscriber => {
+        subscriber.next('a');
+
+        return {
+          unsubscribe: onSubscribe,
+        };
+      });
+    }, { maxAge: 1 });
+
+    const promise = f(2).pipe(toArray()).toPromise();
+    await delay(2);
+    f(2).pipe(toArray()).toPromise();
+
+    const out = await promise;
+    expect(out).toEqual(['a']);
+    expect(onSubscribe).toHaveBeenCalledTimes(1);
   });
 });
